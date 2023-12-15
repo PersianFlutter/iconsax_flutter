88e849456cd5cf6dd2d9dccf8904173039fdcee5
mahmoud <eslamimahmoud98@gmail.com>
2023-12-15 21:30:11 +0330

    feat: removed cached file and add dependencies version

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Iconsax',
+      title: 'Iconsax Flutter',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         fontFamily: GoogleFonts.lato().fontFamily,
